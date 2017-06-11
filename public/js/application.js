cfcd03362368876e00d026d89bca15f53a870307
Joshua Britton <email": null>
2017-06-11 17:00:27 -0500

    form showing on front page

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
 
+$("#add-horse-form").on("submit", function(event){
+  event.preventDefault();
+  // alert('what?')
+
+    var $form = $(this);
+
+
+    $.ajax({
+      method: $form.attr('method'),
+      url: $form.attr('action'),
+    })
+
+     .done(function(response) {
+      $('.container').append(response);
+      $form.hide();
+    })
+
 });
+});
+
+$(".container").on("submit", "#add-horse-form" )

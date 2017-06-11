2921b1f0f4dc4d1def9b58a21295bf35ad9df1f6
Joshua Britton <email": null>
2017-06-11 17:09:41 -0500

    add new horse from form to front page

--- public/js/application.js
+++ public/js/application.js
@@ -18,6 +18,26 @@ $("#add-horse-form").on("submit", function(event){
     })
 
 });
-});
 
-$(".container").on("submit", "#add-horse-form" )
+
+$(".container").on("submit", "#add-horse-form", function(event){
+  event.preventDefault();
+
+  var $form = $(this);
+  var method = $form.attr("method");
+  var action = $form.attr("action");
+  var formData = $form.serialize();
+
+  $.ajax({
+    method: method,
+    action: action,
+    data: formData
+  })
+
+  $.done(function(responseHTML){
+    $('horse-list').append(responseHTML)
+    $('.add-a-horse').hide();
+    $('#add-horse-form').show();
+  })
+})
+});
